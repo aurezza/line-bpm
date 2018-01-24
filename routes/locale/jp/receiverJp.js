878bbf121f3cc5652ba7fb5c1ba167fcc66295a1
Leuther <lklmojica@tmj.ph>
2018-01-24 16:04:30 +0800

    edit receiver

--- routes/locale/jp/receiverJp.js
+++ routes/locale/jp/receiverJp.js
@@ -1,13 +1,19 @@
 var japanese = {
-    Approve: "承認する",
-    Decline: "低下",
-    Name: "名",
-    OvertimeDate: "残業日",
-    OvertimeTime: "残業の時間",
-    OverTimeReason: "残業理由",
-    Approved:"承認された",
-    Declined:"拒否した",
-    Status:"状態"
+                text:{ 
+                    Name: "名",
+                    OvertimeDate: "残業日",
+                    OvertimeTime: "残業の時間",
+                    OverTimeReason: "残業理由",
+                    Approved:"承認された",
+                    Declined:"拒否した",
+                    Status:"状態" 
+                },
+                label:{    
+                    Approve: "承認する",
+                    Decline: "低下"
+                 }
+   
 }
+
 
 module.exports = japanese;
--- routes/locale/localechecker.js
+++ routes/locale/localechecker.js
@@ -1,22 +1,6 @@
-var japanese = require('../locale/jp/receiverJp');
-var english = require('../locale/en/receiverEng');
-
-function localechecker(){      
-        text = {  
-            Name:japanese.Name,
-            OvertimeDate:japanese.OvertimeDate,
-            OvertimeTime:japanese.OvertimeTime,
-            OverTimeReason:japanese.OverTimeReason,
-            Approved:japanese.Approved,
-            Declined:japanese.Declined,
-            Status:japanese.Status
-                };
-        label = {
-            Approve:japanese.Approve,
-            Decline:japanese.Decline,
-            };     
-
-    var localetext = {text:text,label:label};
+function localechecker(){  
+    var japanese = require('../locale/jp/receiver');    
+    var localetext = {text:japanese.text,label:japanese.label};
     return localetext;
 }
 

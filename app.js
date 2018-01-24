32be85d2c48956a52697d336348c1dc59ba2874c
Leuther <lklmojica@tmj.ph>
2018-01-24 16:07:47 +0800

    edit receiverjp and localechecker

--- app.js
+++ app.js
@@ -65,9 +65,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-  // log to file
-  winLogger.error(err);
-  winLogger.log(err);
 
   // render the error page
   res.status(err.status || 500);

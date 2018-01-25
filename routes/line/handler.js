8f62acdfad27f20b1bb88f24a43c2ae3b9422025
Leuther <lklmojica@tmj.ph>
2018-01-25 15:17:39 +0800

    added axios to test

--- routes/line/handler.js
+++ routes/line/handler.js
@@ -7,6 +7,34 @@ function handler(router, axios, querystring, client){
 
         if(eventType == "follow") scanQrCode(client,line_userId);
         res.send(true)
+
+        if(req.body.events[0].postback != null && req.body.events[0].message == null){
+            var parsedData = querystring.parse(req.body.events[0].postback.data);
+            var repeatCounter = 0;
+            (function resend(){
+                setTimeout(callAxios,6000,resend);
+            })();
+
+            function callAxios(resend){
+                axios.post(process.env.REPLYURL_TO_QUESTETRA,
+                    querystring.stringify({
+                        processInstanceId:parsedData.processInstanceId,
+                        key:process.env.KEY_TO_QUESTETRA,
+                        q_replymessage:parsedData.q_replymessage
+                    }))
+                    .then(function(response){
+                            console.log('success');                
+                    })            
+                    .catch(function(error){
+                            // console error here
+                            console.log('failed');
+                            if(repeatCounter >= 10) return;
+                            repeatCounter++;
+                            console.log(repeatCounter);
+                            resend();
+                    }); 
+            }
+        }
     });
 }
 

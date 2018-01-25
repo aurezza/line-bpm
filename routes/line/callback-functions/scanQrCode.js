44b60611bfdead081b5540cebd99468762604146
Leuther <lklmojica@tmj.ph>
2018-01-25 10:53:49 +0800

    scan QR code

--- routes/line/callback-functions/scanQrCode.js
+++ routes/line/callback-functions/scanQrCode.js
@@ -1,20 +1,6 @@
 function scanQrCode(eventType,client,line_userId)
 {
-    console.log("scan qr code is fired");
-    if(eventType == "follow"){
-        const message = {
-            type: 'text',
-            text: 'Hello! \n Thanks for adding BPMS-Messaging Bot \n To proceed please login to https://bpms-messaging.com/'+req.body.events[0].source.userId,
-          };
-          
-        client.pushMessage(line_userId, message)
-            .then(() => {
-                console.losg("message sent to "+ line_userId);    
-            })
-            .catch((err) => {
-                console.log(err)
-            });         
-    }      
+     
 }
 
 module.exports = scanQrCode;
--- routes/line/callback.js
+++ routes/line/callback.js
@@ -6,7 +6,20 @@ function callback(router, axios, querystring, client){
         var line_userId = req.body.events[0].source.userId
         
         scanQrCode(eventType,client,line_userId);
-        
+        if(eventType == "follow"){
+            const message = {
+                type: 'text',
+                text: 'Hello! \n Thanks for adding BPMS-Messaging Bot \n To proceed please login to https://bpms-messaging.com/'+req.body.events[0].source.userId,
+              };
+              
+            client.pushMessage(line_userId, message)
+                .then(() => {
+                    console.losg("message sent to "+ line_userId);    
+                })
+                .catch((err) => {
+                    console.log(err)
+                });         
+        }         
     	if(req.body.events[0].postback != null && req.body.events[0].message == null){
             var parsedData = querystring.parse(req.body.events[0].postback.data);
             var repeatCounter = 0;

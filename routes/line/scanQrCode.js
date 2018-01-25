680a3634a74399101b605b4aa295171fe9d1c7b3
Leuther <lklmojica@tmj.ph>
2018-01-25 13:38:53 +0800

    rename line receiver to handler and remove folder

--- routes/line/scanQrCode.js
+++ routes/line/scanQrCode.js
@@ -1,4 +1,4 @@
-var localechecker = require('../../locale/localechecker');
+var localechecker = require('../locale/localechecker');
 function scanQrCode(client,line_userId)
 {
     var localeText = localechecker('jp','scanQrCode');

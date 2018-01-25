a67e5079ecc38c0a47a20e50a470892e6b94365f
Leuther <lklmojica@tmj.ph>
2018-01-25 08:44:20 +0800

    encapsulate template content

---
+++ routes/questetra/messagetext/messageContent.js
@@ -0,0 +1,24 @@
+var localechecker = require('../../locale/localechecker');
+function messageContent(body) 
+{
+    var localeText = localechecker('jp');
+
+    var messageTemplate = {
+            text :  localeText.text.Name +" : " + body.user_name + "\n"+
+                    localeText.text.OvertimeDate +" : " + body.overtime_date + "\n"+
+                    localeText.text.OvertimeTime +" : " + body.overtime_time + "\n"+
+                    localeText.text.OverTimeReason +" : " + body.overtime_reason + "\n",
+            status : {
+                Approved:localeText.text.Status +" : " +localeText.text.Approved,
+                Declined:localeText.text.Status +" : " +localeText.text.Declined
+                },
+            label : {
+                Approve:localeText.label.Approve,
+                Decline:localeText.label.Decline
+                }
+            }
+
+    return messageTemplate;
+}
+
+module.exports = messageContent;
--- routes/questetra/receiver.js
+++ routes/questetra/receiver.js
@@ -1,39 +1,36 @@
 'use strict';
-var messageText = require('./messagetext/messageBodyText');
-var labelText = require('./messagetext/labelBody');
-var messageStatus = require('./messagetext/messageStatus');
+var messageContent = require('./messagetext/messageContent');
 function receiver(router, client){
   router.post('/receiveFromQuest', function(req, res) {
-    var messageText = messageBodyText(req.body);
-    var labelText = labelText();
-    var messageStatus = messageStatus();
+    var messageContent = messageContent(req.body);
+    console.log("messageContent",messageContent);
     var lineId = 'U34f149724f23c004673a3e11409ed3c0';
-    const message = {
-      "type": "template",
-      "altText": "this is a confirm template",
-      "template": {
-          "type": "confirm",
-          "text":  messageText,
-          "actions": [
-              {
-                "type": "postback",
-                "label": labelText.Approve,
-                "text":  messageText+
-                         messageStatus.Approved,
-                "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=yes"
-              },
-              {
-                "type": "postback",
-                "label": labelText.Decline,
-                "text":  messageText+
-                         messageStatus.Declined,
-                "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=no"
+    // const message = {
+    //   "type": "template",
+    //   "altText": "this is a confirm template",
+    //   "template": {
+    //       "type": "confirm",
+    //       "text":  messageContent,
+    //       "actions": [
+    //           {
+    //             "type": "postback",
+    //             "label": labelText.Approve,
+    //             "text":  messageContent+
+    //                      messageStatus.Approved,
+    //             "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=yes"
+    //           },
+    //           {
+    //             "type": "postback",
+    //             "label": labelText.Decline,
+    //             "text":  messageText+
+    //                      messageStatus.Declined,
+    //             "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=no"
                 
-              }
-          ]
-      }
+    //           }
+    //       ]
+    //   }
     
-    }
+    // }
     client.pushMessage(lineId, message)
         .then(() => {
           // getting the message recieved from questetra and passing to line API 

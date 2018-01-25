628e8d8c472d1c891af04d5171fc61714d2b923c
Leuther <lklmojica@tmj.ph>
2018-01-25 10:14:01 +0800

    scan QR code

--- routes/line/callback-functions/scanQrCode.js
+++ routes/line/callback-functions/scanQrCode.js
@@ -6,9 +6,9 @@ function scanQrCode(object)
             text: 'Hello! \n Thanks for adding BPMS-Messaging Bot \n To proceed please login to https://bpms-messaging.com/'+req.body.events[0].source.userId,
           };
           
-        object.client.pushMessage(objet.line_userId, message)
+        object.client.pushMessage(object.line_userId, message)
             .then(() => {
-                console.log("message sent to "+ line_userId);    
+                console.log("message sent to "+ object.line_userId);    
             })
             .catch((err) => {
                 console.log(err)

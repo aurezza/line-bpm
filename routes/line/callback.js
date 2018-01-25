1e471fd260e0d86a8212d2bb16c92c912b7d7d82
Leuther <lklmojica@tmj.ph>
2018-01-25 12:12:34 +0800

    scan-qr-code

--- routes/line/callback.js
+++ routes/line/callback.js
@@ -4,8 +4,8 @@ function callback(router, axios, querystring, client){
 
         var eventType = req.body.events[0].type
         var line_userId = req.body.events[0].source.userId
-        
-        if(eventType == "follow") scanQrCode(eventType,client,line_userId);
+
+        if(eventType == "follow") scanQrCode(client,line_userId);
 
     	if(req.body.events[0].postback != null && req.body.events[0].message == null){
             var parsedData = querystring.parse(req.body.events[0].postback.data);

4c65fef9f7c6e7ca37f6edd1b88d664f2e008d17
Leuther <lklmojica@tmj.ph>
2018-01-25 08:56:27 +0800

    encapsulate template content

--- routes/questetra/receiver.js
+++ routes/questetra/receiver.js
@@ -5,32 +5,32 @@ function receiver(router, client){
     var textContent = messageContent(req.body);
     console.log("messageContent",textContent);
     var lineId = 'U34f149724f23c004673a3e11409ed3c0';
-    // const message = {
-    //   "type": "template",
-    //   "altText": "this is a confirm template",
-    //   "template": {
-    //       "type": "confirm",
-    //       "text":  textContent.text,
-    //       "actions": [
-    //           {
-    //             "type": "postback",
-    //             "label": labelText.Approve,
-    //             "text":  textContent.text+
-    //                      messageStatus.Approved,
-    //             "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=yes"
-    //           },
-    //           {
-    //             "type": "postback",
-    //             "label": labelText.Decline,
-    //             "text":  textContent+
-    //                      messageStatus.Declined,
-    //             "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=no"
+    const message = {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text":  textContent.text,
+          "actions": [
+              {
+                "type": "postback",
+                "label": textContent.label.Approve,
+                "text":  textContent.text+
+                         textContent.status.Approved,
+                "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=yes"
+              },
+              {
+                "type": "postback",
+                "label": textContent.label.Decline,
+                "text":  textContent.text+
+                         textContent.status.Declined,
+                "data": "processInstanceId="+req.body.process_id+"&key=NKOmgMAo36gnNvVnQwyKNojRwKh4gte0&q_replymessage=no"
                 
-    //           }
-    //       ]
-    //   }
+              }
+          ]
+      }
     
-    // }
+    }
     client.pushMessage(lineId, message)
         .then(() => {
           // getting the message recieved from questetra and passing to line API 

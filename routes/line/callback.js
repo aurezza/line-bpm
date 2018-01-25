850137fff8bece12b5a1be7f7b027a293fae1eb7
Leuther <lklmojica@tmj.ph>
2018-01-25 10:52:22 +0800

    scan QR code

--- routes/line/callback.js
+++ routes/line/callback.js
@@ -5,8 +5,8 @@ function callback(router, axios, querystring, client){
         var eventType = req.body.events[0].type
         var line_userId = req.body.events[0].source.userId
         
-        var a = scanQrCode(eventType,client,line_userId);
-        console.log(a);
+        scanQrCode(eventType,client,line_userId);
+        
     	if(req.body.events[0].postback != null && req.body.events[0].message == null){
             var parsedData = querystring.parse(req.body.events[0].postback.data);
             var repeatCounter = 0;

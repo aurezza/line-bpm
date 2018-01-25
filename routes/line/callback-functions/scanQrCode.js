2f00c4b55296ff450186bbf720e65dc0dd2981ba
Leuther <lklmojica@tmj.ph>
2018-01-25 12:21:05 +0800

    scan-qr-code

--- routes/line/callback-functions/scanQrCode.js
+++ routes/line/callback-functions/scanQrCode.js
@@ -1,18 +1,20 @@
 function scanQrCode(client,line_userId)
 {
     console.log("fire");
-    const message = {
-        type: 'text',
-        text: 'Hello! \n Thanks for adding BPMS-Messaging Bot \n To proceed please login to https://bpms-messaging.com/'+req.body.events[0].source.userId,
-        };
+    console.log("client",client);
+    console.log("line_userId",line_userId);
+    // const message = {
+    //     type: 'text',
+    //     text: 'Hello! \n Thanks for adding BPMS-Messaging Bot \n To proceed please login to https://bpms-messaging.com/'+req.body.events[0].source.userId,
+    //     };
         
-    client.pushMessage(line_userId, message)
-        .then(() => {
-            console.losg("message sent to "+ line_userId);    
-        })
-        .catch((err) => {
-            console.log(err)
-        });         
+    // client.pushMessage(line_userId, message)
+    //     .then(() => {
+    //         console.log("message sent to "+ line_userId);    
+    //     })
+    //     .catch((err) => {
+    //         console.log(err)
+    //     });         
           
 }
 
--- routes/line/callback.js
+++ routes/line/callback.js
@@ -5,21 +5,21 @@ function callback(router, axios, querystring, client){
         var eventType = req.body.events[0].type
         var line_userId = req.body.events[0].source.userId
 
-        // if(eventType == "follow") scanQrCode(client,line_userId);
-        if(eventType == "follow"){
-            const message = {
-                type: 'text',
-                text: 'Hello! \n Thanks for adding BPMS-Messaging Bot \n To proceed please login to https://bpms-messaging.com/'+req.body.events[0].source.userId,
-                };
+        if(eventType == "follow") scanQrCode(client,line_userId);
+        // if(eventType == "follow"){
+        //     const message = {
+        //         type: 'text',
+        //         text: 'Hello! \n Thanks for adding BPMS-Messaging Bot \n To proceed please login to https://bpms-messaging.com/'+req.body.events[0].source.userId,
+        //         };
                 
-            client.pushMessage(line_userId, message)
-                .then(() => {
-                    console.losg("message sent to "+ line_userId);    
-                })
-                .catch((err) => {
-                    console.log(err)
-                });    
-        }
+        //     client.pushMessage(line_userId, message)
+        //         .then(() => {
+        //             console.log("message sent to "+ line_userId);    
+        //         })
+        //         .catch((err) => {
+        //             console.log(err)
+        //         });    
+        // }
     	if(req.body.events[0].postback != null && req.body.events[0].message == null){
             var parsedData = querystring.parse(req.body.events[0].postback.data);
             var repeatCounter = 0;

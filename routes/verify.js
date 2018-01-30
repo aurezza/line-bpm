32f207aa97742ce8aeeee7e5c02dcb9dad0cd097
au <aldunque@tmj.ph>
2018-01-30 10:40:41 +0800

    added locale text for placeholders in verify page

--- routes/verify.js
+++ routes/verify.js
@@ -1,10 +1,17 @@
+var localeChecker = require('./locale/localechecker');
 function verify(router, lineID) {
     // use passport authentication module 
     // http://www.passportjs.org/
     router.get('/verify/:line_id', function(req, res) {
         var lineID = req.params.line_id;
+        var localeText= localeChecker('jp','verify-content');
         res.render('verify', {
-            title: 'Verification for BPMS App',
+            title: localeText.pageTitle.title,
+            panelTitle: localeText.label.panelTitle,
+            verifyButtonText: localeText.button.verify,
+            rememberMeText: localeText.label.rememberMe,
+            usernamePlaceholder: localeText.placeHolder.username, 
+            passwordPlaceholder: localeText.placeHolder.password,
             lineID: lineID
         });
    });

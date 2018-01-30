111ee85f87a2226cf7a5468895d137b01407502d
Leuther <lklmojica@tmj.ph>
2018-01-30 09:41:44 +0800

    testing axios

--- routes/questetra/receiver.js
+++ routes/questetra/receiver.js
@@ -37,24 +37,39 @@ function receiver(object){
           ]
       }    
     };
-    object.client.pushMessage(managerData.line_id, message)
-        .then(() => {
-          setTimeout(object.axios.post(process.env.REPLYURL_TO_QUESTETRA,
-            object.querystring.stringify({
-                processInstanceId:req.body.process_id,
-                key:process.env.KEY_TO_QUESTETRA_REQUEST_STATUS
-            }))
-            .then(function(response){
-                    console.log('success sending reply status');                
-            })            
-            .catch(function(error){
-                    console.log('failed sending reply status');
-            }),6000);
-  
-        })
-        .catch((err) => {
-          console.log("error",err);
-        });
+
+    var throttleCounter = 0;
+    //1000 = 1sec
+    var replyDelayTime = 6000;
+
+    (function resendReplyToQuestetra(){
+        setTimeout(postReplyToQuestetra,replyDelayTime,resendReplyToQuestetra);
+    })();
+
+    function postReplyToQuestetra(resendReplyToQuestetra){
+      object.axios.post(process.env.REPLYURL_TO_QUESTETRA,
+        object.querystring.stringify({
+            processInstanceId:req.body.process_id,
+            key:process.env.KEY_TO_QUESTETRA_REQUEST_STATUS
+        }))
+        .then(function(response){
+            object.client.pushMessage(managerData.line_id, message)
+            .then(() => {
+              console.log("message sent");
+            })
+            .catch((err) => {
+              console.log("error","error sending to client");
+            });
+              console.log('success sending reply status');                
+        })            
+        .catch(function(error){
+              console.log('failed');
+              if(throttleCounter >= 10) return;
+              throttleCounter++;
+              resendReplyToQuestetra();
+        });   
+  }
+
       res.send(true);
 
   });

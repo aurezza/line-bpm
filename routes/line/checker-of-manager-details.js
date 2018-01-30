981b794e1e7a5419f4681a29c13948b1fde664be
Leuther <lklmojica@tmj.ph>
2018-01-30 15:07:59 +0800

    manager-details

---
+++ routes/line/checker-of-manager-details.js
@@ -0,0 +1,22 @@
+var axios = require('axios');
+var querystring = require('querystring');
+var replyToQuestetra = require('./reply-to-questetra');
+
+function checkManagerDetails(managerData, instanceId){
+    
+    if (!Object.keys(managerData).length){
+        var axiosParameters = {
+            url: process.env.REPLYURL_TO_QUESTETRA_REQUEST_STATUS,
+            content:{
+                processInstanceId:instanceId,
+                key:process.env.KEY_TO_QUESTETRA_REQUEST_STATUS,
+                //q_sendingstatus from questetra also be used as query params
+                q_sendingstatus:'no manager detail'
+            }
+        };
+        replyToQuestetra(querystring, axios, 'emptyPostBack', axiosParameters);
+        return false;
+    } 
+    return true;
+}
+module.exports = checkManagerDetails;
--- routes/questetra/receiver.js
+++ routes/questetra/receiver.js
@@ -1,6 +1,7 @@
 'use strict';
 var messageContent = require('./message-text/message-content');
 var fromNode = require('../line/from-node');
+var checkManagerDetails = require('../line/checker-of-manager-details');
 function receiver(router, client, axios, querystring){
   router.post('/receiveFromQuest', function(req, res) {
     var messageText = messageContent(req.body);
@@ -13,40 +14,41 @@ function receiver(router, client, axios, querystring){
         line_id:"U309ccccafe5e38419bcc10c23b117620"
     };
     //<-------------------------------------------->
-    const message = {
-      "type": "template",
-      "altText": "this is a confirm template",
-      "template": {
-          "type": "confirm",
-          "text":  messageText.text,
-          "actions": [
-              {
-                "type": "postback",
-                "label": messageText.label.approve,
-                "text":  messageText.text +
-                         messageText.status.approved,
-                "data": "processInstanceId="+req.body.process_id+"&q_replymessage=yes"
-              },
-              {
-                "type": "postback",
-                "label": messageText.label.decline,
-                "text":  messageText.text +
-                         messageText.status.declined,
-                "data": "processInstanceId="+req.body.process_id+"&q_replymessage=no"
-              }
-          ]
-      }    
-    };
-    
-    client.pushMessage(managerData.line_id, message)
-    .then(() => { 
-        fromNode(querystring, axios, req.body.process_id, 'yes'); 
-    })
-    .catch((err) => { 
-        fromNode(querystring, axios, req.body.process_id, 'no');         
-    });
-    res.send(true);
-
+    if(checkManagerDetails(managerData, req.body.process_id)){
+        const message = {
+            "type": "template",
+            "altText": "this is a confirm template",
+            "template": {
+                "type": "confirm",
+                "text":  messageText.text,
+                "actions": [
+                    {
+                      "type": "postback",
+                      "label": messageText.label.approve,
+                      "text":  messageText.text +
+                               messageText.status.approved,
+                      "data": "processInstanceId="+req.body.process_id+"&q_replymessage=yes"
+                    },
+                    {
+                      "type": "postback",
+                      "label": messageText.label.decline,
+                      "text":  messageText.text +
+                               messageText.status.declined,
+                      "data": "processInstanceId="+req.body.process_id+"&q_replymessage=no"
+                    }
+                ]
+            }    
+          };
+          
+          client.pushMessage(managerData.line_id, message)
+          .then(() => { 
+              fromNode(querystring, axios, req.body.process_id, 'yes'); 
+          })
+          .catch((err) => { 
+              fromNode(querystring, axios, req.body.process_id, 'no');         
+          });
+          res.send(true);
+    }
   });
 }
 module.exports = receiver;

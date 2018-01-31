7c6a3ca94ade4941dbccbcc5cb264782533d791c
Leuther <lklmojica@tmj.ph>
2018-01-31 10:06:49 +0800

    done refactoring and testing

--- routes/questetra/receiver.js
+++ routes/questetra/receiver.js
@@ -1,14 +1,13 @@
 'use strict';
-
 var checkManagerDetails = require('../line/checker-of-manager-details');
 function receiver(router, client){
     router.post('/receiveFromQuest', function(req, res) {
         //Change this to the object retrieved from database
         var managerData = {
-            // name: "Aurezza Lyn Dunque",
-            // email : "aldunque@tmj.ph",
-            // employee_id : "6",
-            // line_id:"U309ccccafe5e38419bcc10c23b117620"
+            name: "Aurezza Lyn Dunque",
+            email : "aldunque@tmj.ph",
+            employee_id : "6",
+            line_id:"U309ccccafe5e38419bcc10c23b117620"
         };
         //<-------------------------------------------->
         checkManagerDetails(managerData, req.body, client); 

3c8accb1b8e26eafaff05549195bd730ed51a0ad
au <aldunque@tmj.ph>
2018-02-01 09:42:57 +0800

    edited scan-qr-code.js text url

--- routes/locale/jp/scan-qr-code.js
+++ routes/locale/jp/scan-qr-code.js
@@ -1,5 +1,5 @@
 var japanese = {
     text: "こんにちは！\n"+"追加ありがとう BPMS-Messaging Bot \n" + 
-    "続行するにはログインしてください https://bpms-messaging.com/verify/"
+    "続行するにはログインしてください https://bot-dev.tmjp.jp/verify/"
 }
 module.exports = japanese;

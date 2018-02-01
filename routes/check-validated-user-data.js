cd0a1e7c17686754c99dbed84cb99364d7e1334f
au <aldunque@tmj.ph>
2018-02-01 16:26:04 +0800

    converted if in checkValidatedUserData to guarding

--- routes/check-validated-user-data.js
+++ routes/check-validated-user-data.js
@@ -1,6 +1,7 @@
 var retrieveUsers = require('./retrieve-users'); 
 var verifyUserWithLineId = require('./verify-user-with-line-id');
 var localeChecker = require('./locale/locale-checker');
+var logger = require('../logger');
 
 var employeeDetails = {};
 
@@ -8,43 +9,37 @@ function checkValidatedUserData(passport, req, res, lineID, validatedUserData) {
     // check if user is in local db
     var users = retrieveUsers(lineID, 'empty');
 
-    if (validatedUserData) {
-        users.then(function(users){
-            if (users){
-                logger.info("The line ID:", lineID, "is already verified");
-                // add error handler on form
-                res.send(localeText.error.lineIdAlreadyExists); 
+    if (!validatedUserData) return logger.error("User data not validated");
+    users.then(function(users){
+        if (users){
+            logger.info("The line ID:", lineID, "is already verified");
+            return res.send(localeText.error.lineIdAlreadyExists); 
+        }
+        // authenticate start
+        passport.authenticate('tmj', function(err, user, info) {
+            var throwErr = err || info;         
+            if (throwErr) {
+                return res.status(400).send(throwErr);            
             }
-            else {
-                // authenticate start
-                passport.authenticate('tmj',
-                function(err, user, info) {
-                    var throwErr = err || info;         
-                    if (throwErr) {
-                        return res.status(400).send(throwErr);        
-                    }
-            
-                    req.logIn(user, function(err) {
-                        if (err) {
-                            // edit message for error; must be generic
-                            return res.status(400).send(err.message);                      
-                        }
-                        employeeDetails = {
-                            lineID: lineID,
-                            name: user.name,
-                            employee_id: user.employee_id,
-                            email: user.email
-                        };
-                        verifyUserWithLineId(employeeDetails, res);
-                    });
-                    
-                })(req,res);
-            }
-        })
-        .catch(function(err){
-            logger.error(err);
-        });
-    }   
+            req.logIn(user, function(err) {
+                if (err) {
+                    return res.status(400).send(err.message);                      
+                }
+                
+                employeeDetails = {
+                    lineID: lineID,
+                    name: user.name,
+                    employee_id: user.employee_id,
+                    email: user.email
+                };
+
+                verifyUserWithLineId(employeeDetails, res);
+            });
+        })(req,res);               
+    })
+    .catch(function(err){
+        logger.error(err);
+    });     
 }
 
 module.exports = checkValidatedUserData;  

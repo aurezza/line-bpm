86cc0af6373c7a7c3bad543d093f73482c09c285
au <aldunque@tmj.ph>
2018-02-01 13:39:42 +0800

    edited logger path

--- routes/retrieve-user-by-emp-id.js
+++ routes/retrieve-user-by-emp-id.js
@@ -1,5 +1,5 @@
 var userModel = require('../models/user-model');
-var logger = require('.././logger');
+var logger = require('../logger');
 function retrieveUserByEmployeeId(receivedEmployeeID) {
 
 	var users = userModel.findOne({employee_id: receivedEmployeeID});

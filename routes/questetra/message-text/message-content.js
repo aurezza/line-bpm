da6da0ec0a76f8cef1d527371324ce68a4330d88
Leuther <lklmojica@tmj.ph>
2018-02-01 13:13:48 +0800

    fix spaces from 2 to 4

--- routes/questetra/message-text/message-content.js
+++ routes/questetra/message-text/message-content.js
@@ -4,19 +4,19 @@ function messageContent(body)
     var localeText = localeChecker('jp','message-content');
 
     var messageTemplate = {
-            text :  localeText.text.name +" : " + body.user_name + "\n"+
-                    localeText.text.overtimeDate +" : " + body.overtime_date + "\n"+
-                    localeText.text.overtimeTime +" : " + body.overtime_time + "\n"+
-                    localeText.text.overTimeReason +" : " + body.overtime_reason + "\n",
-            status : {
+        text :  localeText.text.name +" : " + body.user_name + "\n"+
+                localeText.text.overtimeDate +" : " + body.overtime_date + "\n"+
+                localeText.text.overtimeTime +" : " + body.overtime_time + "\n"+
+                localeText.text.overTimeReason +" : " + body.overtime_reason + "\n",
+        status : {
                 approved:localeText.text.status +" : " +localeText.text.approved,
                 declined:localeText.text.status +" : " +localeText.text.declined
-                },
-            label : {
+        },
+        label : {
                 approve:localeText.label.approve,
                 decline:localeText.label.decline
-                }
-            }
+        }
+    }
 
     return messageTemplate;
 }

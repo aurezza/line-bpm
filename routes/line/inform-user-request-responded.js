6b53fd3e93f33db75c30767a398e73e8b87377d1
Leuther <lklmojica@tmj.ph>
2018-02-08 16:56:30 +0800

    request save update send

--- routes/line/inform-user-request-responded.js
+++ routes/line/inform-user-request-responded.js
@@ -2,8 +2,8 @@
 var sendResponded = require('./send-responded');
 var updateRequestStatus = require('./update-request-status');
 // function informUserRequestResponded(retrievedRequestData,client,line_userId,parsedData){
-    function informUserRequestResponded(retrievedRequestData){    
-        console.log("Object",Object.keys(retrievedRequestData).length);
+    function informUserRequestResponded(data){    
+        console.log("Object",Object.keys(data).length);
     // console.log("client",client);
     // console.log("line_userId",line_userId);
     // console.log("parsedData",parsedData);

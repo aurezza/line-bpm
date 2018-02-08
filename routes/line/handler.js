89a5716ee396a90ec151ea8caa2b14eff97ad35f
Leuther <lklmojica@tmj.ph>
2018-02-08 12:28:18 +0800

    test prevent event

--- routes/line/handler.js
+++ routes/line/handler.js
@@ -5,6 +5,7 @@ var retrieveUserByLineId = require('.././retrieve-user-by-line-id');
 
 function handler(router, axios, querystring, client){
     router.post('/handler', function(req, res) {
+            req.body.events[0].type.messsage = '';
             var eventType = req.body.events[0].type;
             if(eventType=="message"){
                 return ;

d7dc7f7223587dfeb1dac34f8ad3a4e3a0739f4f
Leuther <lklmojica@tmj.ph>
2018-02-08 15:08:23 +0800

    request save update send

--- routes/update-request.js
+++ routes/update-request.js
@@ -2,13 +2,24 @@ var requestModel = require('../models/request-model');
 var logger = require('../logger');
 
 function updateRequest(params) {
-    request.update({ process_id: params.processInstanceId }, 
+
+    // create instance of model transactionModel
+    var newRequest = new requestModel();
+    
+    newRequest.user_name = params.user_name;
+    newRequest.overtime_date = params.overtime_date;
+    newRequest.process_id = params.process_id;
+    newRequest.reason = params.reason;
+    newRequest.status = params.q_replymessage;
+    newRequest.manager_email = params.manager_email;
+
+    newRequest.update({ process_id: newRequest.processInstanceId }, 
         { $set: { 
-                user_name : params.user_name,
-                overtime_date : params.overtime_date,
-                reason : params.overtime_reason,
-                status : params.q_replymessage ,
-                manager_email : params.manager_email,
+                user_name : newRequest.user_name,
+                overtime_date : newRequest.overtime_date,
+                reason : newRequest.overtime_reason,
+                status : newRequest.q_replymessage ,
+                manager_email : newRequest.manager_email,
                 }
     });
     logger.info("updated");

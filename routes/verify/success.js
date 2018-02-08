7ec4e0c0c244ef6f036981c2b8b0c6ebe8c2061a
au <aldunque@tmj.ph>
2018-02-08 13:08:15 +0800

    test added function for window.close

--- routes/verify/success.js
+++ routes/verify/success.js
@@ -3,11 +3,12 @@ function success(router, lineID) {
     router.get('/success/:line_id', function(req, res) {
         var localeText = localeChecker('jp','success-message');
         var lineID = req.params.line_id;
+        var botID = '@nnd9511o';
         res.render('success', {
             title: localeText.successTextTitle, 
             description: localeText.successTextMessage,
             successButtonText: localeText.closeWindow,
-            lineId: lineID
+            lineId: botID
         });
    });
 }

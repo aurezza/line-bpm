2d4dc6a917e41cade58b9e143172f6b9626e3adc
Leuther <lklmojica@tmj.ph>
2018-02-08 16:00:21 +0800

    request save update send

--- routes/update-request.js
+++ routes/update-request.js
@@ -2,10 +2,14 @@ var requestModel = require('../models/request-model');
 var logger = require('../logger');
 
 function updateRequest(params) {
-
+    var replymessage = params.q_replymessage
+    var requestStatus = {
+        "yes":"Approve",
+        "no":"Declined"
+    }
     console.log("id",params.processInstanceId);
     requestModel.update({ process_id: params.processInstanceId }, 
-        { $set: {status : params.q_replymessage}},
+        { $set: {status : requestStatus.replymessage}},
         function(){
             logger.info("updated");
         });

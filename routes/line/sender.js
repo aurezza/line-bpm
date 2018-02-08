ad50f5ace478975228aff0e960868820244efd40
Leuther <lklmojica@tmj.ph>
2018-02-08 13:32:21 +0800

    test prevent event

--- routes/line/sender.js
+++ routes/line/sender.js
@@ -7,14 +7,6 @@ var retrieveRequest = require('../retrieve-request');
 function sender(body, managerData, client){
     console.log("body",body);
     var messageText = messageContent(body);
-        saveRequest({
-            user_name:body.user_name,
-            overtime_date:body.overtime_date,
-            process_id:body.process_id,
-            reason:body.overtime_reason, 
-            response:'pending',
-            manager_email:body.manager_email,       
-        });
         const message = {
             "type": "template",
             "altText": "this is a confirm template",
@@ -45,9 +37,25 @@ function sender(body, managerData, client){
           };
             client.pushMessage(managerData.line_id, message)
             .then(() => { 
+                saveRequest({
+                    user_name:body.user_name,
+                    overtime_date:body.overtime_date,
+                    process_id:body.process_id,
+                    reason:body.overtime_reason, 
+                    response:'failed',
+                    manager_email:body.manager_email,       
+                });
                 fromNode(querystring, axios,body.process_id, 'yes'); 
             })
-            .catch((err) => { 
+            .catch((err) => {
+                saveRequest({
+                    user_name:body.user_name,
+                    overtime_date:body.overtime_date,
+                    process_id:body.process_id,
+                    reason:body.overtime_reason, 
+                    response:'failed',
+                    manager_email:body.manager_email,       
+                }); 
                 fromNode(querystring, axios, body.process_id, 'no');         
             });            
 

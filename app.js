f9b395dba257daa03cb2f0db9b693b8533c91bcf
Leuther <lklmojica@tmj.ph>
2018-02-08 13:07:22 +0800

    test prevent event

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ app.use(cookieSession({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use('/', handler);
-
+//]===================
+//app.use('/', handler);
+//]===================
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');

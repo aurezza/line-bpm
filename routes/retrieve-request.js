7ad9fd6bb72442b89b546b6888f7e337c0040bef
Leuther <lklmojica@tmj.ph>
2018-02-09 08:39:26 +0800

    test

--- routes/retrieve-request.js
+++ routes/retrieve-request.js
@@ -1,12 +1,12 @@
 var requestModel = require('../models/request-model');
 
 function retrieveRequest(id) {
-    var overtimeRequest = requestModel.findOne({process_id: "510",
+    var overtimeRequest = requestModel.findOne({process_id: id,
         $or:[{status: "Approved"},{status: "Declined"}]});
     
     overtimeRequest
     .exec(function(res, err){
-        console.log("overtimeRequest",overtimeRequest)
+        
     });	
 		  
     return overtimeRequest;

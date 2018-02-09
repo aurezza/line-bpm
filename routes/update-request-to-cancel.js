c548b047c2d1f1a7d37b10fcdd0993816d28b4b7
Leuther <lklmojica@tmj.ph>
2018-02-09 16:15:42 +0800

    update status to cancel

--- routes/update-request-to-cancel.js
+++ routes/update-request-to-cancel.js
@@ -3,15 +3,16 @@ var requestModel = require('../models/request-model');
 var logger = require('../logger');
 
 function updateRequesttoCancel(params) {
-    
+
     requestModel.update({ process_id: params.process_id }, 
         { $set: {status : "cancelled"}},
 
         function(){
-            logger.info("process_id :"+params.processInstanceId+" was updated to cancel");
+            logger.info("process_id :"+params.process_id+" was updated to cancel");
+            console.log('params',params);
         });
 
-        console.log('params',params);
+        
     
 }
 

7859e876a224913d148ed8db7bb46eff912fd31e
Leuther <lklmojica@tmj.ph>
2018-02-09 16:58:58 +0800

    final push

--- routes/line/send-responded.js
+++ routes/line/send-responded.js
@@ -1,8 +1,18 @@
 var translations = require("../locale/locale-checker")
 function sendResponded(retrievedRequestData,client,line_userId){
-    console.log("retrievedRequestData",retrievedRequestData);
+    
     var response = translations('jp','responded-message');
+
+    var messageType = {
+        Approved:"responded",
+        Decline:"responded",
+        cancelled:"cancelled"
+    }
+    console.log("retrievedRequestData",retrievedRequestData.status);
     //var messageResponse = response()
+
+
+
     const message = {
         type: 'text',
         text: response,

23c3a2f04ead0f66800df0c1d10a4283d0251c3e
Leuther <lklmojica@tmj.ph>
2018-02-09 08:46:14 +0800

    test

--- routes/line/inform-user-request-responded.js
+++ routes/line/inform-user-request-responded.js
@@ -2,7 +2,6 @@ var sendResponded = require('./send-responded');
 var updateRequestStatus = require('./update-request-status');
 
 function informUserRequestResponded(retrievedRequestData,client,line_userId,parsedData){    
-    console.log("retrievedRequestData",retrievedRequestData);
     if(retrievedRequestData != null) return sendResponded(client,line_userId);
     updateRequestStatus(parsedData);
     

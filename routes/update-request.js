7b5a4c578d2fb4f88f90d928bf13248bb6c7295d
Leuther <lklmojica@tmj.ph>
2018-02-09 16:43:42 +0800

    update statuses

--- routes/update-request.js
+++ routes/update-request.js
@@ -3,30 +3,18 @@ var requestModel = require('../models/request-model');
 var logger = require('../logger');
 
 function updateRequest(params) {
-    console.log('params',params.processInstanceId);
-    console.log('params',params.q_replymessage);
-    
     var replymessage = params.q_replymessage ;
     var requestStatus = {
         yes:"Approved",
         no:"Declined"
     };
     requestModel
-    .findOneAndUpdate({process_id: params.processInstanceId, status: "cancelled"},
+    .findOneAndUpdate({process_id: params.processInstanceId, status: "pending"},
     {status : requestStatus[replymessage]},
     function(){
-        logger.info('updated')
+        logger.info('updated');
     }
     );
-
-       
-
-
-    // requestModel.update({ process_id: params.processInstanceId }, 
-    //     { $set: {status : requestStatus[replymessage]}},
-    //     function(){
-    //         logger.info("process_id :"+params.processInstanceId+" was updated");
-    //     });
     
 }
 

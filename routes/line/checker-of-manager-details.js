b54945ce0d620ead3a2a3c7344b1d0fb89f7dd69
Leuther <lklmojica@tmj.ph>
2018-02-09 10:01:22 +0800

    test email null

--- routes/line/checker-of-manager-details.js
+++ routes/line/checker-of-manager-details.js
@@ -6,19 +6,21 @@ var sender = require('./sender');
 
 function checkManagerDetails(managerData, body, client){
     console.log("in checkManagerDetails");
-    if (Object.keys(managerData).length) return sender(body, managerData, client);
-        logger.error("no manager data retrieved");
-        var axiosParameters = {
-            url: process.env.REPLYURL_TO_QUESTETRA_REQUEST_STATUS,
-            content:{
-                processInstanceId:body.process_id,
-                key:process.env.KEY_TO_QUESTETRA_REQUEST_STATUS,
-                //q_sendingstatus from questetra also be used as query params
-                q_sendingstatus:'no manager detail'
-            }
-        };
+    console.log("managerData",managerData);
+    console.log(Object.keys(managerData).length);
+    // if (Object.keys(managerData).length) return sender(body, managerData, client);
+    //     logger.error("no manager data retrieved");
+    //     var axiosParameters = {
+    //         url: process.env.REPLYURL_TO_QUESTETRA_REQUEST_STATUS,
+    //         content:{
+    //             processInstanceId:body.process_id,
+    //             key:process.env.KEY_TO_QUESTETRA_REQUEST_STATUS,
+    //             //q_sendingstatus from questetra also be used as query params
+    //             q_sendingstatus:'no manager detail'
+    //         }
+    //     };
 
-        replyToQuestetra(querystring, axios, 'emptyPostBack', axiosParameters);
+    //     replyToQuestetra(querystring, axios, 'emptyPostBack', axiosParameters);
     
 }
 module.exports = checkManagerDetails;

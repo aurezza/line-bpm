5dad1857f84672f04e9859128d244c7cda6890ac
Leuther <lklmojica@tmj.ph>
2018-02-12 12:18:08 +0800

    draft

---
+++ routes/line/sender-cancelled-request.js
@@ -0,0 +1,17 @@
+var localeChecker = require('../locale/locale-checker');
+
+function sendCancelledRequest(body,client){
+        // const message = {
+        //     type: 'text',
+        //     text: req.body,
+        //     };
+        // client.pushMessage(line_userId, message)
+        //     .then(() => {
+        //         logger.info("message sent to "+ line_userId);    
+        //     })
+        //     .catch((err) => {
+        //         logger.error(err);
+        //     });
+}
+
+module.exports = sendCancelledRequest;
--- routes/questetra/receiver-cancelled-request.js
+++ routes/questetra/receiver-cancelled-request.js
@@ -1,9 +1,15 @@
 'use strict';
 var updateRequestToCancel = require('../update-request-to-cancel');
-
+var sendCancelledRequest = require('../line/send-cancelled-request');
 function receiverCancelledRequest(router, client){
     router.post('/receiverCancelledRequest', function(req, res) {
         updateRequestToCancel(req.body);
+
+        console.log("body",req.body);
+
+
+
+
     });
 }
 

bc1f89af81b1b6a0cd9151d8253d06d274fa65b4
Leuther <lklmojica@tmj.ph>
2018-02-12 12:44:45 +0800

    test

--- routes/questetra/receiver-cancelled-request.js
+++ routes/questetra/receiver-cancelled-request.js
@@ -1,6 +1,6 @@
 'use strict';
 var updateRequestToCancel = require('../update-request-to-cancel');
-//var sendCancelledRequest = require('../line/send-cancelled-request');
+var sendCancelledRequest = require('../line/sender-cancelled-request');
 function receiverCancelledRequest(router, client){
     router.post('/receiverCancelledRequest', function(req, res) {
         updateRequestToCancel(req.body);

ab1218b6b1fdf681a17d756d70390ae473d28826
Leuther <lklmojica@tmj.ph>
2018-02-14 15:27:42 +0800

    unauthorized access

--- routes/verify/index.js
+++ routes/verify/index.js
@@ -18,35 +18,35 @@ function verify(router, lineBotId) {
                     lineBotId: lineBotId
                 })
             }
-                console.log("accessPass",accessPass);
-                var localeText= localeChecker('jp','verify-content');
-                logger.info("verify page has loaded...");
-                var users = retrieveUsers(lineID, 'empty');
-                users.then(function(users){
-                    if (users){
-                        logger.warn("The line ID:", lineID, "is already verified");
-                        return res.render('verify-error', {
-                            message: localeText.errorMessageLineIdExists,
-                            backButtonText: localeText.button.back,
-                            lineBotId: lineBotId
-                        })
-                    }
-                    res.render('verify', {
-                        title: localeText.pageTitle.title,
-                        panelTitle: localeText.label.panelTitle,
-                        verifyButtonText: localeText.button.verify,
-                        usernamePlaceholder: localeText.placeHolder.username, 
-                        passwordPlaceholder: localeText.placeHolder.password,
-                        lineID: lineID,
-                        verified: false,
-                        errors: {},
-                        customError: ''   
-                    });               
-        
-                })
-                .catch(function(err){
-                    logger.error(err);;
-                }); 
+            console.log("accessPass",accessPass);
+            var localeText= localeChecker('jp','verify-content');
+            logger.info("verify page has loaded...");
+            var users = retrieveUsers(lineID, 'empty');
+            users.then(function(users){
+                if (users){
+                    logger.warn("The line ID:", lineID, "is already verified");
+                    return res.render('verify-error', {
+                        message: localeText.errorMessageLineIdExists,
+                        backButtonText: localeText.button.back,
+                        lineBotId: lineBotId
+                    })
+                }
+                res.render('verify', {
+                    title: localeText.pageTitle.title,
+                    panelTitle: localeText.label.panelTitle,
+                    verifyButtonText: localeText.button.verify,
+                    usernamePlaceholder: localeText.placeHolder.username, 
+                    passwordPlaceholder: localeText.placeHolder.password,
+                    lineID: lineID,
+                    verified: false,
+                    errors: {},
+                    customError: ''   
+                });               
+    
+            })
+            .catch(function(err){
+                logger.error(err);;
+            }); 
 
 
         })

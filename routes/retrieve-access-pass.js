ce0142f3916e38944fc994ae29bf080ddbe66d9e
Leuther <lklmojica@tmj.ph>
2018-02-14 17:00:22 +0800

    test

--- routes/retrieve-access-pass.js
+++ routes/retrieve-access-pass.js
@@ -4,7 +4,7 @@ function retrieveAccessPass(lineId, token){
         // convert save code above to promise
 		var accessPass = accessPassModel.findOne(
 			{owner: lineId},
-            {access_pass_token: token},
+            {access_pass_token: "https://bot-dev.tmjp.jp/verify/da35778644a35082ee630dc17619a9e3e8f184003c18d6fa5804d89231871885/U69d42742a42fbb532dd02920351e776eaaa"},
             {status:"active"}
             
 		);

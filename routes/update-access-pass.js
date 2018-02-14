d506553b2f1c0927de7679e14af6193d01fa49a7
Leuther <lklmojica@tmj.ph>
2018-02-14 16:25:14 +0800

    problema mo

--- routes/update-access-pass.js
+++ routes/update-access-pass.js
@@ -7,7 +7,7 @@ function updateAccessPass(lineId) {
     console.log("lineId in updateAccessPass",lineId);
     accessPassModel.update({ owner: "U69d42742a42fbb532dd02920351e776e" }, 
         { $set: {status : "expired"}},
-
+        
         function(){
             logger.info("all access pass with the :"+lineId+" owner was updated to expired");
         });

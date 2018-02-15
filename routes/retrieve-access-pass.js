0f2bedd17959e8cd9027ea78e0690e87a2a93802
Leuther <lklmojica@tmj.ph>
2018-02-15 08:55:25 +0800

    testing

--- routes/retrieve-access-pass.js
+++ routes/retrieve-access-pass.js
@@ -4,7 +4,7 @@ function retrieveAccessPass(lineId, token){
         // convert save code above to promise
 		var accessPass = accessPassModel.findOne(
 			{owner: lineId,
-            access_pass_token: "token",
+            access_pass_token: token,
             status:"active"}
             
 		);

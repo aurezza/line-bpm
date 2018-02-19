6f6a6d06098a4191ee120e49a1207267a399972c
Leuther <lklmojica@tmj.ph>
2018-02-19 09:58:34 +0800

    test

--- routes/retrieve-access-pass-line-id.js
+++ routes/retrieve-access-pass-line-id.js
@@ -2,13 +2,14 @@
 var accessPassModel = require('../models/access-pass-model');
 var logger = require('../logger');
 function retrieveAccessPassOwner(lineId){
-        // convert save code above to promise
+		// convert save code above to promise
+		console.log('here1');
 		var accessPassOwner = accessPassModel.findOne(
 			{line_id: lineId,
             status:"active"}
             
 		);
-		console.log('here');
+		console.log('here2');
 		
 		accessPassOwner
 		.exec(function(res, err){

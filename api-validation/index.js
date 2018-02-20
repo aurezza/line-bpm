aae4aa1e93ade62b7e2035bd8c9ba3253493e3fb
au <aldunque@tmj.ph>
2018-02-20 12:32:44 +0800

    modified index.js in api validation for testing

--- api-validation/index.js
+++ api-validation/index.js
@@ -7,35 +7,44 @@ var whitelistForCors = require('../whitelist-for-cors');
 var cors = require('cors');
 
 function apiValidation(router) {
-    var whitelist = [process.env.APP_URL, 'http://localhost:8080'];
+    router.use(function(req, res, next){
+        req.headers.origin = req.headers.origin || req.headers.host;
+        next();
+    });
+    var whitelist = [process.env.APP_URL];
     var corsOptions = {
-        // origin: function(origin, callback) {
-        //     if(whitelist.indexOf(origin) !== -1){
+       origin: function(origin, callback) {
+            if(whitelistForCors.indexOf(origin) !== -1){
                 
-        //         logger.info('successfully allowed by CORS');
-        //         return callback(null, true);
-        //     } else {
-        //         logger.info(whitelist);
-        //         logger.info(whitelist.indexOf(origin));
-        //         logger.error('cors error');
-        //         callback(new Error('Not allowed by CORS'));
-        //     }
-        // },
-        origin: "https://bot-dev3.tmjp.jp", // enable for testing only
+                logger.info('successfully allowed by CORS');
+                return callback(null, true);
+            } else {
+                logger.info(whitelistForCors);
+                logger.info(whitelistForCors.indexOf(origin));
+                logger.info("result: ", whitelistForCors.indexOf(origin) !== -1);
+                logger.info("origin: ", origin);
+                logger.error('cors error');
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        // origin: "*", // enable for testing only
         credentials: true,
-        preflightContinue: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
         methods: 'GET, POST'
     }
 
-    router.use(cors(corsOptions), function(req,res, next) {
+    router.use(cors(corsOptions), function(req, res, next) {
         logger.info('passed cors...');
         logger.info('request headers: ', req.headers);
+        // res.setHeader("Access-Control-Allow-Origin", process.env.APP_URL);
+        // res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         next();
     });
 
     router.get('/testGet', function(req, res) {
         logger.info('test get got accessed');
-        res.send('testing for ');
+        res.status(200).send('testing for ');
     });
 
     router.put('/testPut', function(req, res) {
--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const client = new line.Client(config);
 // app.use(cors());
 
 // app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Origin", process.env.APP_URL);
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   res.header('Access-Control-Allow-Methods', 'GET, POST');
 //   res.header('Access-Control-Allow-Credentials', false);

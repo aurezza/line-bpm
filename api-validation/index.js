8551095cba6dc276e60c1a1638af56272b8cd771
au <aldunque@tmj.ph>
2018-02-20 13:46:50 +0800

    removed test routes and created new file for storing cors options

--- api-validation/index.js
+++ api-validation/index.js
@@ -3,49 +3,13 @@
 var logger = require('../logger');
 var verifyToken = require('./verify-token');
 var kernel = require('../kernel');
-var whitelistForCors = require('../whitelist-for-cors');
+var corsOptions = require('./cors-options');
 var cors = require('cors');
 
 function apiValidation(router) {
     router.use(function(req, res, next){
         // solution provided in https://github.com/expressjs/cors/issues/71
         req.headers.origin = req.headers.origin || req.headers.host;
-        next();
-    });
-    
-    var corsOptions = {
-       origin: function(origin, callback) {
-            logger.info("origin: ", origin);
-            var whiteList = whitelistForCors.find(function(listedUrl) {
-                return listedUrl.includes(origin);
-            });
-            logger.info("whiteList: ", whiteList);
-            logger.info("whitelistForCors: ", whitelistForCors);
-            logger.info("indesof origin: ", whitelistForCors.indexOf(origin));
-            logger.info("result: ", whitelistForCors.indexOf(origin) !== -1);
-            if(whiteList){
-                logger.info('successfully allowed by CORS');
-                return callback(null, true);
-            } 
-            logger.error('cors error');
-            callback(new Error('Not allowed by CORS'));
-        },
-        credentials: true,
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        methods: 'GET, POST'
-    }
-
-    router.get('/testGet', function(req, res) {
-        res.send("test get");
-    });
-
-    router.put('/testPut', function(req, res) {
-        res.send("test put");
-    });
-
-    router.use(cors(corsOptions), function(req, res, next) {
-        logger.info('passed cors...');
-        logger.info('request headers: ', req.headers);
         next();
     });
 

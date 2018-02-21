96bbed76b162764beeb66e60ebd7cf6e5a21ac29
au <aldunque@tmj.ph>
2018-02-21 12:29:18 +0800

    re-install modules

--- api-validation/generate-token.js
+++ api-validation/generate-token.js
@@ -3,29 +3,33 @@
 var jwt = require('jsonwebtoken');
 var logger = require('../logger');
 var retrieveByApiKey = require('./retrieve-by-api-key');
+var Token = require('../routes/node/token-generator');
+
+var token = new Token(process.env.APP_SECRET_KEY);
 
 // TODO: create proper UI for generating keys
 function generateToken(router) {
-    router.get('/api/generate-token', function(req, res) {
+    router.get('/api/generate-token/:api_name', function(req, res) {
         // TODO: add authorization handler
         var successVerification = true;
+        var dateNow = Date();
 
         // TODO: create separate? payload for questetra
         const payload = {
             api_name: 'questetra', // change this to either line/quest on request atm
-            created_at: 1518657215797
-            // insert other defined values here
+            created_at: dateNow
         }; 
 
-        var key = 'somethingjibberish'; // TODO: generate dynamic request from db
+        var key = token.get(); 
         var secretKey = process.env.APP_SECRET_KEY;
-        var createdAt = 1518657215797; // refer to api db for this value
 
-        const generatedSecretKey = key + secretKey + createdAt;
+        const generatedSecretKey = key + secretKey + dateNow;
     
         logger.info('generatedkey: ', generatedSecretKey);
 
         var token = jwt.sign(payload, generatedSecretKey);
+
+        // TODO: save generated token and payload details to db
 
         // return the token information 
         if(successVerification){

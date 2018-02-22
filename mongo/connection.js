caca7724111e7acb55cc09db1b702efe5c73f730
au <aldunque@tmj.ph>
2018-02-22 12:12:24 +0800

    changed mongo connection for server testing

--- mongo/connection.js
+++ mongo/connection.js
@@ -2,7 +2,9 @@
 function connection(mongoose, connectionURL){
 	mongoose.Promise = global.Promise;
 	// remove deprecationWarning error
-	mongoose.connect(connectionURL);
+	mongoose.connect(connectionURL, {
+		useMongoClient: true
+	});
 	// get the default connection//localhost
 	var db = mongoose.connection;
 	// notification error for mongodb errors

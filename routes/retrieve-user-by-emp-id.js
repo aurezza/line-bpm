26632189d0651c8057fb6b55c867df2080b31c14
Leuther <lklmojica@tmj.ph>
2018-03-02 12:14:46 +0800

    test

--- routes/retrieve-user-by-emp-id.js
+++ routes/retrieve-user-by-emp-id.js
@@ -2,7 +2,7 @@
 var userModel = require('../models/user-model');
 var logger = require('../logger');
 function retrieveUserByEmployeeId(receivedEmployeeID) {
-
+    console.log("receivedEmployeeID", receivedEmployeeID);
     var users = userModel.findOne({employee_id: receivedEmployeeID});
     
     users

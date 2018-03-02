9228b731805cb415060e9174418367bbd7ce4161
Leuther <lklmojica@tmj.ph>
2018-03-02 12:12:47 +0800

    test

--- routes/verify/verify-user-with-line-id.js
+++ routes/verify/verify-user-with-line-id.js
@@ -9,12 +9,12 @@ var Users = require('../../users/users');
 
 function verifyUserWithLineId(employeeDetails, res, client, lineID, lineBotId) {
     var localeText = localeChecker('jp', 'verify-content');
+    console.log("employeeDetails", employeeDetails);
     var userWithLineId = retrieveUserByEmployeeId(employeeDetails.employee_id);
 
     userWithLineId.then(function(userWithLineId) {
         if (!userWithLineId) {
             var user = new Users();
-            console.log("employeeDetails", employeeDetails);
             user.save(employeeDetails);
             // saveUser(employeeDetails, logger);
             successVerifyLineMessage(client, lineID);
@@ -37,7 +37,6 @@ function verifyUserWithLineId(employeeDetails, res, client, lineID, lineBotId) {
 
     })
         .catch(function(error) {
-            console.log("here?");
             logger.error(error.message);     
         });                        
 }

1cc23811262ed71cb142c1712bec46b47d6e60fa
au <aldunque@tmj.ph>
2018-03-06 08:53:27 +0800

    added missing Users variable

--- routes/verify/index.js
+++ routes/verify/index.js
@@ -1,35 +1,37 @@
-// var retrieveUsers = require('../retrieve-users');
+var Users = require('../../service/users');
 var localeChecker = require('../locale/locale-checker');
 var logger = require('../../logger');
 function verify(router, lineBotId) {
     router.get('/verify/:line_id', function(req, res) {
+        var user = new Users();
         var lineID = req.params.line_id;
         var localeText = localeChecker('jp', 'verify-content');
         logger.info("verify page has loaded...");
 
-        var users = retrieveUsers(lineID, 'empty');
-        users.then(function(users) {
-            if (users) {
-                logger.warn("The line ID:", lineID, "is already verified");
-                return res.render('verify-error', {
-                    message: localeText.errorMessageLineIdExists,
-                    backButtonText: localeText.button.back,
-                    lineBotId: lineBotId
-                })
-            }
-            res.render('verify', {
-                title: localeText.pageTitle.title,
-                panelTitle: localeText.label.panelTitle,
-                verifyButtonText: localeText.button.verify,
-                usernamePlaceholder: localeText.placeHolder.username, 
-                passwordPlaceholder: localeText.placeHolder.password,
-                lineID: lineID,
-                verified: false,
-                errors: {},
-                customError: ''   
-            });
+        var users = user.retrieveUsers(lineID, 'empty');
+        users
+            .then(function(data) {
+                if (data) {
+                    logger.warn("The line ID:", lineID, "is already verified");
+                    return res.render('verify-error', {
+                        message: localeText.errorMessageLineIdExists,
+                        backButtonText: localeText.button.back,
+                        lineBotId: lineBotId
+                    })
+                }
+                res.render('verify', {
+                    title: localeText.pageTitle.title,
+                    panelTitle: localeText.label.panelTitle,
+                    verifyButtonText: localeText.button.verify,
+                    usernamePlaceholder: localeText.placeHolder.username, 
+                    passwordPlaceholder: localeText.placeHolder.password,
+                    lineID: lineID,
+                    verified: false,
+                    errors: {},
+                    customError: ''   
+                });
 
-        })
+            })
             .catch(function(err) {
                 logger.error(err);;
             }); 

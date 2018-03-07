5fa981910a19d01d812947c9cf649fad5ebed8ac
Leuther <lklmojica@tmj.ph>
2018-03-07 14:26:53 +0800

    rename requests

--- routes/questetra/receiver-cancelled-request.js
+++ routes/questetra/receiver-cancelled-request.js
@@ -1,22 +1,22 @@
 'use strict';
 var logger = require('../../logger');
 var errorLocator = require('../node/error-locator');
-var Requests = require('../../service/requests')
+var ServiceRequests = require('../../service/requests')
 var Users = require('../../service/users');
-var Request = require('../../line/request');
+var LineRequest = require('../../line/request');
 function receiverCancelledRequest(router, client) {
     router.post('/receiverCancelledRequest', function(req, res) {
-        var request = new Request();
-        var requests = new Requests ();
+        var lineRequest = new LineRequest();
+        var serviceRequests = new ServiceRequests ();
         var user = new Users();
-        requests.updateToCancel(req.body)
+        serviceRequests.updateToCancel(req.body)
 
         var managerData = {};
         var users = user.retriveByEmpEmail(req.body.manager_email);
         
         users.then(function(users) {
             managerData = users;
-            request.sendCancelledRequest(managerData, req.body, client);  
+            lineRequest.sendCancelledRequest(managerData, req.body, client);  
         })
             .catch(function(error) {
                 logger.error(error.message);

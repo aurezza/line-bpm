7cdba3c3ff177626279586bdebd9e71cd13da6c7
au <aldunque@tmj.ph>
2018-03-07 12:52:56 +0800

    testing verify index

--- routes/verify/index.js
+++ routes/verify/index.js
@@ -2,9 +2,12 @@
 var Verify = require('../../controllers/verify-page');
 var csrf = require('csurf');
 var csrfProtection = csrf({ cookie: true });
+var express = require('express');
+const app = express();
 function verify(router) {
     var verifyFunc = new Verify();
-    router.get('/verify/:token/:line_id', csrfProtection, verifyFunc.showVerifyPage);
+    console.log('vefiry index is up');
+    app.get('/verify/:token/:line_id', csrfProtection, verifyFunc.showVerifyPage);
 }
 
 module.exports = verify;

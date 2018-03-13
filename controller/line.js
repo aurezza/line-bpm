47ceddc3fe2a9e7f6c4e4152f06c5c5232de4bd7
Leuther <lklmojica@tmj.ph>
2018-03-13 09:58:43 +0800

    test controller

--- controller/line.js
+++ controller/line.js
@@ -1,52 +1,53 @@
 'use strict';
 
-// var logger = require('../logger');
-// var errorLocator = require('../routes/node/error-locator');
-// var Users = require('../service/users');
-// var Line = require('../line/line');
-// var line = new Line();
-// var Sender = require('../line/sender');
-// var sender = new Sender();
-// var Node = require('../line/node');
-// var node = new Node();
-
+var logger = require('../logger');
+var errorLocator = require('../routes/node/error-locator');
+var Users = require('../service/users');
+var Line = require('../line/line');
+var line = new Line();
+var Sender = require('../line/sender');
+var sender = new Sender();
+var Node = require('../line/node');
+var node = new Node();
 
 function LineController () {}
 
 LineController.prototype = {
     eventHandler: {
         follow: follow,
-        unfollow: unfollow
+        unfollow: unfollow,
+        message: message,
+        postback: postback
     }
         
 };
 
 function follow(params) {
-    console.log('Line contoller follow')
+    logger.info("follow event triggered");
+    var user = new Users();
+    let line_userId = params.req.events[0].source.userId;
+    var users = user.retrieveByLineId(line_userId);
+    users
+        .then(function (users) {
+            if (users) return  sender.userExist(params.client, line_userId, users.employee_name);
+            line.scanQrCode(params.client, line_userId);
+        })
+        .catch(function (error) {
+            logger.error(error.message);
+            logger.error(errorLocator());
+        });
 }
 
-function unfollow(params) {
-    console.log('Line contoller unfollow')
+function postback(params) {
+    logger.info("postback event triggered");
+    //postBack is data query params depending on manager reply
+    let line_userId = params.req.events[0].source.userId;
+    var postBack = params.req.events[0].postback;
+    node.outgoingMessage(postBack, params.client, line_userId);
 }
 
-// function follow(params) {
-//     var user = new Users();
-//     let line_userId = params.req.events[0].source.userId;
-//     var users = user.retrieveByLineId(line_userId);
-//     users
-//         .then(function (users) {
-//             if (users) return  sender.userExist(params.client, line_userId, users.employee_name);
-//             line.scanQrCode(params.client, line_userId);
-//         })
-//         .catch(function (error) {
-//             logger.error(error.message);
-//             logger.error(errorLocator());
-//         });
-// }
-
-// function unfollow(params) {
-//     logger.info("message event");    
-// }
+function unfollow(params) {logger.info("unfollow event triggered");};
+function message(params) {logger.info("message event triggered");};
 
 
 module.exports = LineController;
--- routes/line/handler.js
+++ routes/line/handler.js
@@ -1,47 +1,11 @@
 'use strict';
-// var logger = require('../../logger');
-// var errorLocator = require('../node/error-locator');
-// var Users = require('../../service/users');
-// var Line = require('../../line/line');
-// var line = new Line();
-// var Sender = require('../../line/sender');
-// var sender = new Sender();
-// var Node = require('../../line/node');
-// var node = new Node();
+var logger = require('../../logger');
+
 var LineController = require('../../controller/line');
-
-// function Controller() {}
-
-// Controller.prototype = {
-//     eventHandler: {
-//         follow: follow,
-//         unfollow: unfollow
-//     }
-    
-// };
-
-// function follow(params) {
-//     var user = new Users();
-//     let line_userId = params.req.events[0].source.userId;
-//     var users = user.retrieveByLineId(line_userId);
-//     users
-//         .then(function (users) {
-//             if (users) return  sender.userExist(params.client, line_userId, users.employee_name);
-//             line.scanQrCode(params.client, line_userId);
-//         })
-//         .catch(function (error) {
-//             logger.error(error.message);
-//             logger.error(errorLocator());
-//         });
-// }
-
-// function unfollow(params) {
-//     logger.info("message event");    
-// }
 
 function handler(router, axios, querystring, client) {
     router.post('/handler', function(req, res) {
-        console.log("LineController", LineController);
+        logger.info('line handler triggered');
         var lineController = new LineController();
         var eventType = req.body.events[0].type;
         lineController.eventHandler[eventType]({
@@ -52,30 +16,5 @@ function handler(router, axios, querystring, client) {
     });
 }
 
-
-// var eventHandler = {};
-// eventHandler.follow = function(params) {
-//     var user = new Users();
-//     let line_userId = params.req.events[0].source.userId;
-//     var users = user.retrieveByLineId(line_userId);
-//     users
-//         .then(function (users) {
-//             if (users) return  sender.userExist(params.client, line_userId, users.employee_name);
-//             line.scanQrCode(params.client, line_userId);
-//         })
-//         .catch(function (error) {
-//             logger.error(error.message);
-//             logger.error(errorLocator());
-//         });
-// }
-// eventHandler.postback = function(params) {
-//     //postBack is data query params depending on manager reply
-//     let line_userId = params.req.events[0].source.userId;
-//     var postBack = params.req.events[0].postback;
-//     node.outgoingMessage(postBack, params.client, line_userId);
-// }
-
-// eventHandler.unfollow = function(params) {logger.info("unfollow event");};
-// eventHandler.message = function(params) {logger.info("message event");};
 
 module.exports = handler;

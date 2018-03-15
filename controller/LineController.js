6875e6817e4e16cad059a09bccba116ffbd92e32
Leuther <lklmojica@tmj.ph>
2018-03-15 08:19:21 +0800

    line event handler

--- controller/LineController.js
+++ controller/LineController.js
@@ -1,11 +1,5 @@
 'use strict';
-
-var logger = require('../logger');
-var errorLocator = require('../node/error-locator');
-var UserModel = require('../model/UserModel');
-var Line = require('../service/Line');
-var Questetra = require('../service/Questetra');
-var LineEventHandler = require('../service/LineEventHandler');
+var LineEventHandler = require('../service/LineEventHandler')
 const line = require('@line/bot-sdk');
 const config = {
     channelAccessToken: process.env.LINE_BOT_CHANNEL_TOKEN,
@@ -24,7 +18,6 @@ LineController.prototype = {
 
 function eventTrigger(req, res) {
     var eventType = req.body.events[0].type;
-
     LineEventHandler().eventHandler[eventType]({
         req: req.body, 
         client: client
@@ -33,32 +26,5 @@ function eventTrigger(req, res) {
     res.send(true);
 }
 
-// var eventHandler = {};
-
-// eventHandler.follow = function (params) {
-//     logger.info("follow event triggered");
-//     let line_userId = params.req.events[0].source.userId;
-//     var users = UserModel().retrieveByLineId(line_userId);
-//     users
-//         .then(function (users) {
-//             if (users) return  Line().userExist(params.client, line_userId, users.employee_name);
-//             Line().scanQrCode(params.client, line_userId);
-//         })
-//         .catch(function (error) {
-//             logger.error(error.message);
-//             logger.error(errorLocator());
-//         });
-// }
-
-// eventHandler.postback = function(params) {
-//     logger.info("postback event triggered");
-//     //postBack is data query params depending on manager reply
-//     let line_userId = params.req.events[0].source.userId;
-//     var postBack = params.req.events[0].postback;
-//     Questetra().outgoingMessage(postBack, params.client, line_userId);
-// }
-
-// eventHandler.unfollow = function(params) {logger.info("unfollow event triggered");};
-// eventHandler.message = function(params) {logger.info("message event triggered");};
 
 module.exports = LineController;
--- service/LineEventHandler.js
+++ service/LineEventHandler.js
@@ -1,4 +1,9 @@
 'use strict';
+var logger = require('../logger');
+var errorLocator = require('../node/error-locator');
+var UserModel = require('../model/UserModel');
+var Line = require('../service/Line');
+var Questetra = require('../service/Questetra');
 
 function LineEventHandler() {
     if (!(this instanceof LineEventHandler)) return new LineEventHandler();
@@ -7,19 +12,37 @@ function LineEventHandler() {
 LineEventHandler.prototype = {
     eventHandler: {
         follow: follow,
-        unfollow: unfollow
+        unfollow: unfollow,
+        postback: postback,
+        message: message
     }
 };
 
 function follow(params) {
+    logger.info("follow event triggered");
     let line_userId = params.req.events[0].source.userId;
-
-    console.log("line_userId", line_userId);
-    console.log("follow")
+    var users = UserModel().retrieveByLineId(line_userId);
+    users
+        .then(function (users) {
+            if (users) return  Line().userExist(params.client, line_userId, users.employee_name);
+            Line().scanQrCode(params.client, line_userId);
+        })
+        .catch(function (error) {
+            logger.error(error.message);
+            logger.error(errorLocator());
+        });
 }
 
-function unfollow(params) {
-    console.log('unfollow')
+function postback(params) {
+    logger.info("postback event triggered");
+    //postBack is data query params depending on manager reply
+    let line_userId = params.req.events[0].source.userId;
+    var postBack = params.req.events[0].postback;
+    Questetra().outgoingMessage(postBack, params.client, line_userId);
 }
+
+function unfollow(params) {logger.info("unfollow event triggered");};
+function message(params) {logger.info("message event triggered");};
+
 
 module.exports = LineEventHandler;

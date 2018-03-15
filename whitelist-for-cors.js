52c14e8d54c445e321a9caa54a5969cc706c752d
au <aldunque@tmj.ph>
2018-03-15 14:36:46 +0800

    removed localhost from whitelist

--- whitelist-for-cors.js
+++ whitelist-for-cors.js
@@ -1,6 +1,6 @@
 'use strict';
 
 // replace process.env.APP_URL with 'http://localhost:8080' for testing
-var whitelistForCors = [process.env.APP_URL, 'http://localhost:8080'];
+var whitelistForCors = [process.env.APP_URL];
 
 module.exports = whitelistForCors;

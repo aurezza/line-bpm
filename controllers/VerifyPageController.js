7ce9955dbc56f1950455c226f91ae35f2e1950be
au <aldunque@tmj.ph>
2018-03-16 12:11:58 +0800

    modify config for testing

--- controllers/VerifyPageController.js
+++ controllers/VerifyPageController.js
@@ -73,6 +73,14 @@ function showVerifyPage (req, res) {
 }
 
 function showVerifySuccess (req, res) {
+    // var client = new line.Client(LineConfiguration().lineConfiguration([0]));
+    const config = {
+        channelAccessToken: process.env.LINE_BOT_CHANNEL_TOKEN,
+        channelSecret: process.env.LINE_BOT_CHANNEL_SECRET,
+    };
+    const client = new line.Client(config);
+    logger.info('lineconfig: ', LineConfiguration().lineConfiguration([0]));
+    logger.info('client', client);
     res.render('success', RenderPage().successForm());
 }
 
@@ -220,9 +228,9 @@ function successVerifyLineMessage(lineID)
         text: msgContent,
     };
 
-    var client = new line.Client(LineConfiguration().lineConfiguration([0]));
+    // var client = new line.Client(LineConfiguration().lineConfiguration([0]));
     
-    client.pushMessage(lineID, message)
+    LineConfiguration().lineConfiguration([0]).pushMessage(lineID, message)
         .then(() => {
             logger.info("message sent to " + lineID);    
         })

37bf8c4d2b6f4653d56444c674ca1f83de79a597
au <aldunque@tmj.ph>
2018-03-16 13:05:18 +0800

    modify cors for testing

--- whitelist-for-cors.js
+++ whitelist-for-cors.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // replace process.env.APP_URL with 'http://localhost:8080' for testing
-var whitelistForCors = [process.env.APP_URL];
+// var whitelistForCors = [process.env.APP_URL];
+var whitelistForCors = [];
 
 module.exports = whitelistForCors;

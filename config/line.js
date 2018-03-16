bbc84a47f5492b1bb87c4c735bcceeaf525ad58d
au <aldunque@tmj.ph>
2018-03-16 12:54:29 +0800

    removed comments

--- config/line.js
+++ config/line.js
@@ -18,11 +18,6 @@ function lineConfiguration() {
             channelSecret: process.env.LINE_BOT_CHANNEL_SECRET
         }
     ];
-    // const config = {
-    //     channelAccessToken: process.env.LINE_BOT_CHANNEL_TOKEN,
-    //     channelSecret: process.env.LINE_BOT_CHANNEL_SECRET,
-    // };
-    // const client = new line.Client(config);
     
     return config;
 }

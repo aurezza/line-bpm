54f25a1df73b18749df19ef4c9786ae137dbc693
au <aldunque@tmj.ph>
2018-03-22 16:54:21 +0800

    testing api controller with Jasmine

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,28 +1,73 @@
-var Request = require("request");
-var App = require('../app');
 
-describe("Server", () => {
-    var server;
-    beforeAll(() => {
-        server = require("../app");
+var Request = require('request');
+var ApiController = require('../controller/ApiController');
+var ApiModel = require('../model/ApiModel');
+// describe("Server", function() {
+//     var Request = require("request");
+//     var server;
+//     beforeAll(function() {
+//         server = require("../app");
+//     });
+//     afterAll(function() {
+//         server.close();
+//     });
+//     describe("GET /", function() {
+//         var data = {};
+//         beforeAll((done) => {
+//             Request.get("http://localhost:8080/", (error, response, body) => {
+//                 data.status = response.statusCode;
+//                 data.body = body;
+//                 done();
+//             });
+//         });
+//         // it("Status 200", function() {
+//         //     expect(data.status).toBe(200);
+//         // });
+//         // it("Body", function() {
+//         //     expect(data.body).toBe("The Polyglot Developer");
+//         // });
+//     });
+// });
+
+// describe("Server - Jasmine", function() {
+    
+//     var server;
+    
+//     beforeEach(function() {
+//         server = require('../app');
+//     });
+//     afterAll(function() {
+//         server.close();
+//     });
+//     describe("Server status", function() {
+//         var data = {};
+//         beforeAll(function(done) {
+//             Request.get("http://localhost:8080", function(error, response, body) {
+//                 data.status = response.statusCode;
+//                 data.body =  body;
+//                 done();
+//             })
+//         });
+//         it("status 200", function() {
+//             expect(data.status).toBe(200);
+//         });
+//     });
+
+// });
+
+describe("ApiController", function() {
+    // 
+    var api;
+    // var ApiController;
+    // var ApiController = require('../controller/ApiController');
+    beforeEach(function() {
+        // ApiController = require('../controller/ApiController');
+        // api = new ApiController();
     });
-    afterAll(() => {
-        server.close();
+    it("test", function() {
+        spyOn(ApiController(), 'corsOptions');
+        ApiController().model;
+        expect(ApiController().model).toBe(ApiModel());
     });
-    describe("GET /", () => {
-        var data = {};
-        beforeAll((done) => {
-            Request.get("http://localhost:8080/", (error, response, body) => {
-                data.status = response.statusCode;
-                data.body = body;
-                done();
-            });
-        });
-        // it("Status 200", () => {
-        //     expect(data.status).toBe(200);
-        // });
-        // it("Body", () => {
-        //     expect(data.body).toBe("The Polyglot Developer");
-        // });
-    });
+
 });

4ee29501c5b8b32e8d34442804a0d4c32d878efb
au <aldunque@tmj.ph>
2018-04-18 14:35:08 +0800

    removed commented code

--- routes/app.js
+++ routes/app.js
@@ -9,17 +9,10 @@ var mongoDbURL = "mongodb://" + process.env.MONGODB_URL;
 var mongoDbName = process.env.MONGODB_NAME;
 const connectionURL = mongoDbURL + mongoDbName;
 
-var csrf = require('csurf');
-var csrfProtection = csrf({ cookie: true });
-
-// var kernel = require('./kernel');
 var Middleware = require('../middleware/RouterMiddleware');
 
 var ApiController = require('../controller/ApiController');
 var Api = ApiController();
-
-// var VerifyPageController = require('../controller/VerifyPageController');
-// var Verify = VerifyPageController();
 
 const externalRoutes = ['/api/receiveFromQuest', '/api/handler'];
 
@@ -36,12 +29,6 @@ router.use(externalRoutes, Api.corsOptions(), Middleware().checkOrigin);
 passportTmj();
 
 // internal page
-// router.get('/verify/:token/:line_id', csrfProtection, Verify.showPage.bind(Verify));
-// router.post('/verify/:token/:line_id', Verify.expressValidator(), csrfProtection, Verify.checkFormData.bind(Verify)); 
-// router.get('/generate-token/:api_name', Api.generateToken.bind(Api));
-// router.get('/success', Verify.showSuccess.bind(Verify));
-
-// testing for Routes service
 var routeService = Routes(router);
 // insert middlewares into an array
 routeService.get('/verify/:token/:line_id', 'verify@showPage', ['csrfProtection']);

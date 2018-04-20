9b602ef61102d19f5e9777a1885c8b0addf3f5e7
au <aldunque@tmj.ph>
2018-04-20 09:03:58 +0800

    cleaned code

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,4 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
 var Routes = require('../service/Routes');
 var mongoose = require('mongoose');
 var connection = require('../mongo/connection');
@@ -9,28 +7,19 @@ var mongoDbURL = "mongodb://" + process.env.MONGODB_URL;
 var mongoDbName = process.env.MONGODB_NAME;
 const connectionURL = mongoDbURL + mongoDbName;
 
-var Middleware = require('../middleware/RouterMiddleware');
-
-var ApiController = require('../controller/ApiController');
-var Api = ApiController();
-
 const externalRoutes = ['/api/receiveFromQuest', '/api/handler'];
 
-// db connection
 connection(mongoose, connectionURL);
 
-// middleware for all routes
-// router.use([Middleware().setOrigin, Middleware().tokenSyntaxError]);
+// format for use method only: ['middleware'], '<controllerName>@<method>', '/<path name>'  
 Routes.use(['setOrigin', 'tokenSyntaxError']);
 
-// // middleware for external routes
-// router.use(externalRoutes, Api.corsOptions(), Middleware().checkOrigin);
 Routes.use(['checkOrigin', 'tokenSyntaxError'], 'api@corsOptions', externalRoutes);
 
-// passport
+// passport initialize
 passportTmj();
 
-// internal page
+// internal pages
 // format: '/<path name>', '<controllerName>@<method>', ['middleware']
 Routes.get('/verify/:token/:line_id', 'verify@showPage', ['csrfProtection']);
 Routes.post('/verify/:token/:line_id', 'verify@checkFormData', ['expressValidator', 'csrfProtection']); 

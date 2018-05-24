30776d0727d830d5fe0b1ecbd3e6e8edcc90a764
au <aldunque@tmj.ph>
2018-05-24 16:24:59 +0800

    added missing translation files

---
+++ storage/lang/en/line.js
@@ -0,0 +1,26 @@
+module.exports = {
+    url: "Hello！\nThanks for the add BPMS-Messaging Bot \nPlease log in to continue :url",
+    user_exist: "Welcome back :username !\nA good day :D ",
+    responded: "Hello! You have already responded to this request. Thank you：D",
+    cancelled: "This request has already been canceled. For details, see Mail Notification. ",
+    text: { 
+        name: "Name : :username",
+        overtimeDate: "Overtime Date : :overtimedate",
+        overtimeTime: "Overtime Time : :overtimeTime",
+        overTimeReason: "Overtime Reason : :overtimereason",
+        approved: "Approved",
+        declined: "Declined",
+        status: "Status" 
+    },  
+    status: {
+        approved: "Status : Approved",
+        declined: "Status : Declined"
+    },
+    label: {    
+        approve: "Approve",
+        decline: "Decline"
+    },
+    header: {
+        cancelledMessage: "This request with the following details has already been canceled."
+    }
+}
---
+++ storage/lang/en/verify.js
@@ -0,0 +1,26 @@
+module.exports = {
+    successTextMessage: "Your account has been verified!",
+    successTextTitle: "Success!",
+    closeWindow: "Return",
+    errorMessageLineIdExists: "Already confirmed.",
+    button: {
+        verify: "Confirmation",
+        back: "Return"
+    },
+    title: "BPMS Application Verification",
+    label: {
+        panelTitle: "BPMS Verification"
+    },
+    placeHolder: {
+        username: "username", 
+        password: "password"
+    },
+    error: {
+        mustNotBeEmpty: "Field must not be empty",
+        lineIdAlreadyExists: "Already verified.",
+        employeeIdAlreadyExists: "This user has already been confirmed",
+        wrongCredentials: "Wrong credentials",
+        alreadyVerified: "You have already been verified",
+        unauthorizedAccess: "Error : 403 - Unauthorized Access"
+    }
+};
